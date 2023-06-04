f89de56b3e6ba41444551ef3f77d8a0f41763aab
חני בן ציון <chani11275@gmail.com>
2023-06-04 22:01:22 +0300

    my commit

--- src/components/GuidencePage.js
+++ src/components/GuidencePage.js
@@ -24,7 +24,9 @@ export default function GuidencePage(){
             </p>
             <div className="m-3">
             <br/>
-             שים/י לב:<br/>
+             שים/י לב:<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+</svg><br/>
             <ul className="list-inline-item">
                 <li className="m-2">הכנס/י את פרטיך האישיים על פי ההוראות הכתובות בטופס.</li>
                 <li className="m-2">כדי להוסיף ילד לחץ/י על הכפתור +.</li>
